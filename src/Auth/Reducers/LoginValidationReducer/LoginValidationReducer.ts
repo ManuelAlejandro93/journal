5c04287bdcfd4de513655c0b61e8e8cd6133dde1
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-05-04 11:33:34 -0500

    capitulo 283 listo. Reducer de login hecho. Ahora voy a hacer el reducer del register.

--- src/Auth/Reducers/LoginValidationReducer/LoginValidationReducer.ts
+++ src/Auth/Reducers/LoginValidationReducer/LoginValidationReducer.ts
@@ -12,31 +12,31 @@ export const loginValidationReducer = (
   switch (action.type) {
     case 'invalid-email|invalid-password':
       return {
-        hasEmailError: action.payload.emailValidationResult,
-        hasPasswordError: action.payload.passwordValidationResult,
+        hasEmailError: !action.payload.emailValidationResult,
+        hasPasswordError: !action.payload.passwordValidationResult,
         emailErrorMessage:
           'Check your email. It must look like this "usuario123@server.com"',
         passwordErrorMessage: 'Your password must have at least 8 characters'
       };
     case 'invalid-email|valid-password':
       return {
-        hasEmailError: action.payload.emailValidationResult,
-        hasPasswordError: action.payload.passwordValidationResult,
+        hasEmailError: !action.payload.emailValidationResult,
+        hasPasswordError: !action.payload.passwordValidationResult,
         emailErrorMessage:
           'Check your email. It must look like this "usuario123@server.com"',
         passwordErrorMessage: 'Good password. It is strong.'
       };
     case 'valid-email|invalid-password':
       return {
-        hasEmailError: action.payload.emailValidationResult,
-        hasPasswordError: action.payload.passwordValidationResult,
+        hasEmailError: !action.payload.emailValidationResult,
+        hasPasswordError: !action.payload.passwordValidationResult,
         emailErrorMessage: 'Your email looks good, check your password',
         passwordErrorMessage: 'Your password must have at least 8 characters'
       };
     case 'valid-email|valid-password':
       return {
-        hasEmailError: action.payload.emailValidationResult,
-        hasPasswordError: action.payload.passwordValidationResult,
+        hasEmailError: !action.payload.emailValidationResult,
+        hasPasswordError: !action.payload.passwordValidationResult,
         emailErrorMessage: 'Your email looks good',
         passwordErrorMessage: 'Your password looks good'
       };
@@ -44,3 +44,10 @@ export const loginValidationReducer = (
       return { ...state };
   }
 };
+
+//!Documentación:
+
+//? hasEmailError: !action.payload.emailValidationResult,
+//el validador dice si la cadena email o contraseña son adecuadas.
+//lo cuál es lo contrario a tener error en la expresión evaluada.
+//por lo que uno es el contrario del otro.
