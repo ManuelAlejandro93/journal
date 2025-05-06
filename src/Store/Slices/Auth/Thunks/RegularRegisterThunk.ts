b166fcf9b99267d2bc372a2c49e1f2daec4a585a
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-05-06 14:39:15 -0500

    Capitulo 285 -- thunk listo.

--- src/Store/Slices/Auth/Thunks/RegularRegisterThunk.ts
+++ src/Store/Slices/Auth/Thunks/RegularRegisterThunk.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { regularRegister } from '@/Firebase';
 import { RegisterUserInputData } from '@/Interfaces';
+//xxx
 
 export const regularRegisterThunk = createAsyncThunk(
   'auth/regular-register-thunk',
