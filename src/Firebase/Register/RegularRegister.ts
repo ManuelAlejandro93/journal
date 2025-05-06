f4fe4ba3b5cb85b177c52086e028d5a52a424a27
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-05-06 16:06:29 -0500

    Capitulo 286

--- src/Firebase/Register/RegularRegister.ts
+++ src/Firebase/Register/RegularRegister.ts
@@ -4,18 +4,19 @@ import { RegisterUserInputData } from '@/Interfaces';
 
 export const regularRegister = async (userData: RegisterUserInputData) => {
   try {
-    const registerResult = await createUserWithEmailAndPassword(
+    const firstRegisterResult = await createUserWithEmailAndPassword(
       firebaseAuth,
       userData.email,
       userData.password
     );
+
     return {
       ok: true,
       // name: registerResult.user.displayName,
       name: userData.name.trim(),
-      uuid: registerResult.user.uid,
-      photo: registerResult.user.photoURL,
-      email: registerResult.user.email
+      uuid: firstRegisterResult.user.uid,
+      photo: firstRegisterResult.user.photoURL ?? '',
+      email: firstRegisterResult.user.email
     };
   } catch (error) {
     throw error;
