98a87be5706061650eea8025726de80444f7406c
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-05-08 21:04:33 -0500

    cap 288 - Loginpage - se hizo la axn asíncrona.

--- src/Firebase/Login/RegularLogin.ts
+++ src/Firebase/Login/RegularLogin.ts
@@ -1,10 +1,7 @@
-//todo: método para checkear que el usuario existe.
-import {} from 'firebase/auth';
 import { LoginUserInputData } from '@/Interfaces';
 
 export const regularLogin = async (loginUserInputData: LoginUserInputData) => {
   try {
-    //todo axn asíncrona.
     return {
       ok: true,
       name: '',
