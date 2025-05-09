e70abc93aa64c4d864a718d9fb0b01a8d1e748b4
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-05-08 21:41:30 -0500

    cap 288 - Loginpage - se hizo la axn asíncrona.

--- src/Firebase/Login/RegularLogin.ts
+++ src/Firebase/Login/RegularLogin.ts
@@ -1,13 +1,22 @@
+import { signInWithEmailAndPassword, UserCredential } from 'firebase/auth';
 import { LoginUserInputData } from '@/Interfaces';
+import { firebaseAuth } from '@/Firebase';
 
-export const regularLogin = async (loginUserInputData: LoginUserInputData) => {
+export const regularLogin = async (userLoginInputData: LoginUserInputData) => {
   try {
+    const { user }: UserCredential = await signInWithEmailAndPassword(
+      firebaseAuth,
+      userLoginInputData.email,
+      userLoginInputData.password
+    );
+
+    const { displayName, uid, photoURL, email } = user;
     return {
       ok: true,
-      name: '',
-      uuid: '',
-      photo: '',
-      email: ''
+      name: displayName,
+      uuid: uid,
+      photo: photoURL ?? '',
+      email: email
     };
   } catch (error) {
     throw error;
