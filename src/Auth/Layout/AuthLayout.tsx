1038daed99dfdbc460e82b49c7c53327d7cbab02
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-05-12 16:23:34 -0500

    Cap 294 -- Listo.

--- src/Auth/Layout/AuthLayout.tsx
+++ src/Auth/Layout/AuthLayout.tsx
@@ -15,7 +15,7 @@ export const AuthLayout = ({ authPageName, children }: Props) => {
       justifyContent={'center'}
       sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
     >
-      <div className='grid shadow-2xl shadow-black grid-cols-1 rounded-md bg-white p-4 space-x-4'>
+      <div className='grid shadow-2xl shadow-black grid-cols-1 rounded-md bg-white p-4 space-x-4 animate-fade-up'>
         <div className='text-center'>
           <Typography fontSize={'2rem'}>
             {authPageName.toUpperCase()}
--- src/Journal/Layout/JournalLayout.tsx
+++ src/Journal/Layout/JournalLayout.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const JournalLayout = ({ children }: Props) => {
   return (
-    <div className='flex'>
+    <div className='flex animate-jump-in animate-duration-500'>
       <SideBar />
       <Navbar />
       <main className='p-4 mt-20'>{children}</main>
