9d639d35c6c798cb351a6c50984f55fccc34c75f
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-05-24 12:47:48 -0500

    cap301 - camellando.

--- src/Firebase/DB/addNewNote.ts
+++ src/Firebase/DB/addNewNote.ts
@@ -1,18 +1,25 @@
 import { doc, collection, setDoc } from 'firebase/firestore/lite';
 import { firebaseDB } from '@/Firebase';
 
-export const addNewEmptyNote = async (uuid: string): Promise<undefined> => {
+interface Note {
+  noteId: string;
+  title: string;
+  body: string;
+  date: number;
+}
+
+export const addNewEmptyNote = async (uuid: string): Promise<Note> => {
   const newDoc = doc(collection(firebaseDB, `${uuid}/journal/notes`));
 
-  const newNote = {
+  const newNoteTemp = {
     title: '',
     body: '',
     date: new Date().getTime()
   };
 
   try {
-    await setDoc(newDoc, newNote);
-    return;
+    await setDoc(newDoc, newNoteTemp);
+    return { ...newNoteTemp, noteId: newDoc.id };
   } catch (error) {
     throw error;
   }
