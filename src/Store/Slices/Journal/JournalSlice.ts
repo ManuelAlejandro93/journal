4415faaefb0a6fc0d2b8aa48d4a3c66c957da7e8
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-06-04 18:01:13 -0500

    set activeNote listo.

--- src/Store/Slices/Journal/JournalSlice.ts
+++ src/Store/Slices/Journal/JournalSlice.ts
@@ -14,14 +14,25 @@ const journalSlice = createSlice({
   initialState: noteInitialData,
   reducers: {
     setActiveNote(state, action: PayloadAction<Note>) {
+      //Petición http
+      state!.httpInfo.isFetching = false;
+      state!.httpInfo.hasError = false;
+      state!.httpInfo.errorMessage = null;
+
+      //Note informacion
+      state.isSavingInDB = false;
+      state.dbSavingMessage = '';
+      state.isThereActiveNote = state.isThereActiveNote;
+
+      //active note
+      state.activeNote.body = action.payload.body;
       state.activeNote.date = action.payload.date;
-      state.activeNote.noteId = action.payload.noteId;
       state.activeNote.title = action.payload.title;
       state.activeNote.noteId = action.payload.noteId;
-      state.activeNote.body = action.payload.body;
-      state.activeNote.imgUrls = [
-        `Las imgURLS no van a cambiar ni por el putas. ${action.payload.noteId} `
-      ];
+      state.activeNote.imgUrls = action.payload.imgUrls;
+
+      //all notes
+      // noteState.allNotes = noteState.allNotes;
     },
     onChangeActiveNoteTitle(
       state,
@@ -53,178 +64,178 @@ const journalSlice = createSlice({
   extraReducers(builder) {
     builder.addCase(
       addNewEmptyNoteThunk.fulfilled,
-      (noteState, action: PayloadAction<Note>) => {
+      (state, action: PayloadAction<Note>) => {
         //Petición http
-        noteState!.httpInfo.hasError = false;
-        noteState!.httpInfo.errorMessage = null;
-        noteState!.httpInfo.isFetching = false;
+        state!.httpInfo.hasError = false;
+        state!.httpInfo.errorMessage = null;
+        state!.httpInfo.isFetching = false;
 
         //Note informacion
-        noteState.isSavingInDB = false;
-        noteState.dbSavingMessage = '';
-        noteState.isThereActiveNote = true;
+        state.isSavingInDB = false;
+        state.dbSavingMessage = '';
+        state.isThereActiveNote = true;
 
         //active note
-        noteState.activeNote.body = action.payload.body;
-        noteState.activeNote.date = action.payload.date;
-        noteState.activeNote.title = action.payload.title;
-        noteState.activeNote.noteId = action.payload.noteId;
-        noteState.activeNote.imgUrls = action.payload.imgUrls;
+        state.activeNote.body = action.payload.body;
+        state.activeNote.date = action.payload.date;
+        state.activeNote.title = action.payload.title;
+        state.activeNote.noteId = action.payload.noteId;
+        state.activeNote.imgUrls = action.payload.imgUrls;
 
         //all notes
-        noteState.allNotes =
-          !noteState.allNotes.length || noteState.allNotes.length <= 0
+        state.allNotes =
+          !state.allNotes.length || state.allNotes.length <= 0
             ? [action.payload]
-            : [action.payload, ...noteState.allNotes];
+            : [action.payload, ...state.allNotes];
       }
     );
-    builder.addCase(addNewEmptyNoteThunk.rejected, (noteState, action) => {
+    builder.addCase(addNewEmptyNoteThunk.rejected, (state, action) => {
       //Petición http
-      noteState!.httpInfo.hasError = true;
-      noteState!.httpInfo.errorMessage = action.error.message as string;
-      noteState!.httpInfo.isFetching = false;
+      state!.httpInfo.hasError = true;
+      state!.httpInfo.errorMessage = action.error.message as string;
+      state!.httpInfo.isFetching = false;
 
       //Note informacion
-      noteState.isSavingInDB = false;
-      noteState.dbSavingMessage = '';
-      noteState.isThereActiveNote = false;
+      state.isSavingInDB = false;
+      state.dbSavingMessage = '';
+      state.isThereActiveNote = false;
 
       //active note
-      noteState.activeNote.body = null;
-      noteState.activeNote.date = null;
-      noteState.activeNote.title = null;
-      noteState.activeNote.noteId = null;
-      noteState.activeNote.imgUrls = [];
+      state.activeNote.body = null;
+      state.activeNote.date = null;
+      state.activeNote.title = null;
+      state.activeNote.noteId = null;
+      state.activeNote.imgUrls = [];
 
       //all notes
-      noteState.allNotes = [];
+      state.allNotes = [];
     });
-    builder.addCase(addNewEmptyNoteThunk.pending, (noteState) => {
+    builder.addCase(addNewEmptyNoteThunk.pending, (state) => {
       //Petición http
-      noteState!.httpInfo.isFetching = true;
-      noteState!.httpInfo.hasError = false;
-      noteState!.httpInfo.errorMessage = null;
+      state!.httpInfo.isFetching = true;
+      state!.httpInfo.hasError = false;
+      state!.httpInfo.errorMessage = null;
 
       //Note informacion
-      noteState.isSavingInDB = true;
-      noteState.dbSavingMessage = '';
-      noteState.isThereActiveNote = false;
+      state.isSavingInDB = true;
+      state.dbSavingMessage = '';
+      state.isThereActiveNote = false;
 
       //active note
-      noteState.activeNote.body = null;
-      noteState.activeNote.date = null;
-      noteState.activeNote.title = null;
-      noteState.activeNote.noteId = null;
-      noteState.activeNote.imgUrls = [];
+      state.activeNote.body = null;
+      state.activeNote.date = null;
+      state.activeNote.title = null;
+      state.activeNote.noteId = null;
+      state.activeNote.imgUrls = [];
 
       //all notes
-      noteState.allNotes = [];
+      state.allNotes = [];
     });
-    builder.addCase(getAllNotesThunk.fulfilled, (noteState, action) => {
+    builder.addCase(getAllNotesThunk.fulfilled, (state, action) => {
       if (!action.payload || action.payload.length <= 0) {
         //Petición http
-        noteState!.httpInfo.isFetching = false;
-        noteState!.httpInfo.hasError = false;
-        noteState!.httpInfo.errorMessage = null;
+        state!.httpInfo.isFetching = false;
+        state!.httpInfo.hasError = false;
+        state!.httpInfo.errorMessage = null;
 
         //Note informacion
-        noteState.isSavingInDB = false;
-        noteState.dbSavingMessage = '';
-        noteState.isThereActiveNote = false;
+        state.isSavingInDB = false;
+        state.dbSavingMessage = '';
+        state.isThereActiveNote = false;
 
         //active note
-        noteState.activeNote.body = null;
-        noteState.activeNote.date = null;
-        noteState.activeNote.title = null;
-        noteState.activeNote.noteId = null;
-        noteState.activeNote.imgUrls = [];
+        state.activeNote.body = null;
+        state.activeNote.date = null;
+        state.activeNote.title = null;
+        state.activeNote.noteId = null;
+        state.activeNote.imgUrls = [];
 
         //all notes
-        noteState.allNotes = [];
+        state.allNotes = [];
         return;
       }
 
       //Petición http
-      noteState!.httpInfo.hasError = false;
-      noteState!.httpInfo.errorMessage = null;
-      noteState!.httpInfo.isFetching = false;
+      state!.httpInfo.hasError = false;
+      state!.httpInfo.errorMessage = null;
+      state!.httpInfo.isFetching = false;
 
       //Note informacion
-      noteState.isSavingInDB = false;
-      noteState.dbSavingMessage = '';
-      noteState.isThereActiveNote = true;
+      state.isSavingInDB = false;
+      state.dbSavingMessage = '';
+      state.isThereActiveNote = true;
 
       //active note
-      noteState.activeNote.body = action.payload[0].body;
-      noteState.activeNote.date = action.payload[0].date;
-      noteState.activeNote.title = action.payload[0].title;
-      noteState.activeNote.noteId = action.payload[0].noteId;
+      state.activeNote.body = action.payload[0].body;
+      state.activeNote.date = action.payload[0].date;
+      state.activeNote.title = action.payload[0].title;
+      state.activeNote.noteId = action.payload[0].noteId;
 
       //todo: Revisar despues de añadir una nueva nota
-      noteState.activeNote.imgUrls = [];
+      state.activeNote.imgUrls = [];
 
       //todo: Revisar despues de añadir una nueva nota
       //all notes
       if (!action.payload || action.payload.length <= 0) {
-        noteState.allNotes = [];
+        state.allNotes = [];
       } else {
-        noteState.allNotes = [...action.payload];
+        state.allNotes = [...action.payload];
       }
     });
 
-    builder.addCase(getAllNotesThunk.rejected, (noteState, action) => {
+    builder.addCase(getAllNotesThunk.rejected, (state, action) => {
       //Petición http
-      noteState!.httpInfo.hasError = false;
-      noteState!.httpInfo.errorMessage = action.error.message as string;
-      noteState!.httpInfo.isFetching = false;
+      state!.httpInfo.hasError = false;
+      state!.httpInfo.errorMessage = action.error.message as string;
+      state!.httpInfo.isFetching = false;
 
       //Note informacion
-      noteState.isSavingInDB = false;
-      noteState.dbSavingMessage = '';
-      noteState.isThereActiveNote = false;
+      state.isSavingInDB = false;
+      state.dbSavingMessage = '';
+      state.isThereActiveNote = false;
 
       //active note
-      noteState.activeNote.body = null;
-      noteState.activeNote.date = null;
-      noteState.activeNote.title = null;
-      noteState.activeNote.noteId = null;
-      noteState.activeNote.imgUrls = [];
+      state.activeNote.body = null;
+      state.activeNote.date = null;
+      state.activeNote.title = null;
+      state.activeNote.noteId = null;
+      state.activeNote.imgUrls = [];
 
       //all notes
-      noteState.allNotes = [];
+      state.allNotes = [];
     });
 
-    builder.addCase(getAllNotesThunk.pending, (noteState) => {
+    builder.addCase(getAllNotesThunk.pending, (state) => {
       //Petición http
-      noteState!.httpInfo.isFetching = true;
-      noteState!.httpInfo.hasError = false;
-      noteState.httpInfo.errorMessage = null;
+      state!.httpInfo.isFetching = true;
+      state!.httpInfo.hasError = false;
+      state.httpInfo.errorMessage = null;
 
       //Note informacion
-      noteState.isSavingInDB = true;
-      noteState.isThereActiveNote = false;
-      noteState.dbSavingMessage = '';
+      state.isSavingInDB = true;
+      state.isThereActiveNote = false;
+      state.dbSavingMessage = '';
 
       //active note
-      noteState.activeNote.body = null;
-      noteState.activeNote.date = null;
-      noteState.activeNote.title = null;
-      noteState.activeNote.noteId = null;
-      noteState.activeNote.imgUrls = [];
+      state.activeNote.body = null;
+      state.activeNote.date = null;
+      state.activeNote.title = null;
+      state.activeNote.noteId = null;
+      state.activeNote.imgUrls = [];
 
       //all notes
-      noteState.allNotes = [];
+      state.allNotes = [];
     });
-    builder.addCase(updateSingleNoteByIDThunk.fulfilled, (noteState) => {
+    builder.addCase(updateSingleNoteByIDThunk.fulfilled, (state) => {
       //Petición http
-      noteState!.httpInfo.isFetching = false;
-      noteState!.httpInfo.hasError = false;
-      noteState!.httpInfo.errorMessage = null;
+      state!.httpInfo.isFetching = false;
+      state!.httpInfo.hasError = false;
+      state!.httpInfo.errorMessage = null;
 
       //Note informacion
-      noteState.isSavingInDB = false;
-      noteState.dbSavingMessage = 'ok-on-updating-single-note-by-id';
-      noteState.isThereActiveNote = true;
+      state.isSavingInDB = false;
+      state.dbSavingMessage = 'ok-on-updating-single-note-by-id';
+      state.isThereActiveNote = true;
 
       //active note
       // noteState.activeNote.body = noteState.activeNote.body;
@@ -237,16 +248,16 @@ const journalSlice = createSlice({
       // noteState.allNotes = noteState.allNotes;
     });
 
-    builder.addCase(updateSingleNoteByIDThunk.rejected, (noteState, action) => {
+    builder.addCase(updateSingleNoteByIDThunk.rejected, (state, action) => {
       //Petición http
-      noteState!.httpInfo.isFetching = false;
-      noteState!.httpInfo.hasError = true;
-      noteState!.httpInfo.errorMessage = action.error.message as string;
+      state!.httpInfo.isFetching = false;
+      state!.httpInfo.hasError = true;
+      state!.httpInfo.errorMessage = action.error.message as string;
 
       //Note informacion
-      noteState.isSavingInDB = false;
-      noteState.dbSavingMessage = 'error-on-updating-single-note-by-id';
-      noteState.isThereActiveNote = true;
+      state.isSavingInDB = false;
+      state.dbSavingMessage = 'error-on-updating-single-note-by-id';
+      state.isThereActiveNote = true;
 
       //active note
       // noteState.activeNote.body = noteState.activeNote.body;
@@ -259,85 +270,125 @@ const journalSlice = createSlice({
       // noteState.allNotes = noteState.allNotes;
     });
 
-    builder.addCase(updateSingleNoteByIDThunk.pending, (noteState) => {
+    builder.addCase(updateSingleNoteByIDThunk.pending, (state) => {
       //Petición http
-      noteState!.httpInfo.isFetching = true;
-      noteState!.httpInfo.hasError = false;
-      noteState!.httpInfo.errorMessage = null;
+      state!.httpInfo.isFetching = true;
+      state!.httpInfo.hasError = false;
+      state!.httpInfo.errorMessage = null;
 
       //Note informacion
-      noteState.isSavingInDB = false;
-      noteState.dbSavingMessage = '';
-      noteState.isThereActiveNote = false;
+      state.isSavingInDB = false;
+      state.dbSavingMessage = '';
+      state.isThereActiveNote = false;
 
       //active note
-      noteState.activeNote.body = null;
-      noteState.activeNote.date = null;
-      noteState.activeNote.title = null;
-      noteState.activeNote.noteId = null;
-      noteState.activeNote.imgUrls = [];
+      state.activeNote.body = null;
+      state.activeNote.date = null;
+      state.activeNote.title = null;
+      state.activeNote.noteId = null;
+      state.activeNote.imgUrls = [];
 
       //all notes
-      noteState.allNotes = [];
+      state.allNotes = [];
     });
     builder.addCase<any>(
       uploadImageThunk.fulfilled,
-      (
-        noteState: NoteInitialData,
-        action: PayloadAction<CloudinarySuccessed>
-      ) => {
-        //? Http state
-        noteState!.httpInfo.hasError = false;
-        noteState!.httpInfo.errorMessage = null;
-        noteState!.httpInfo.isFetching = false;
-        noteState.dbSavingMessage = 'saving-image-successed';
-        //? active note state
-        if (noteState.activeNote.imgUrls.length === 0) {
-          noteState.activeNote.imgUrls = [action.payload.secure_url];
-        } else {
-          noteState.activeNote.imgUrls = [
-            action.payload.secure_url,
-            ...noteState.activeNote.imgUrls
-          ];
-        }
+      (state: NoteInitialData, action: PayloadAction<CloudinarySuccessed>) => {
+        //Petición http
+        state!.httpInfo.isFetching = false;
+        state!.httpInfo.hasError = false;
+        state!.httpInfo.errorMessage = null;
+
+        //Note informacion
+        state.isSavingInDB = false;
+        state.dbSavingMessage = '';
+        state.isThereActiveNote = true;
+
+        //active note
+        // noteState.activeNote.body = noteState.activeNote.body;
+        // noteState.activeNote.date = noteState.activeNote.date;
+        // noteState.activeNote.title = noteState.activeNote.title;
+        // noteState.activeNote.noteId = noteState.activeNote.noteId;
+        // noteState.activeNote.imgUrls = noteState.activeNote.imgUrls;
+
+        //!cosas que voy a necesitar más adelante. Start
+        // !noteState.activeNote.imgUrls ||
+        // noteState.activeNote.imgUrls.length === 0
+        //   ? [action.payload.secure_url]
+        //   : [action.payload.secure_url, ...noteState.activeNote.imgUrls];
+
+        //all notes
+        // noteState.allNotes = noteState.allNotes;
+        //...................................................................................................................................................................................................................................................................................................................................
+
         //? all notes state
-        noteState.allNotes = noteState.allNotes.map((note) => {
-          if (
-            note.noteId === noteState.activeNote.noteId &&
-            note.imgUrls.length === 0
-          ) {
-            return {
-              ...note,
-              imgUrls: [action.payload.secure_url]
-            };
-          } else if (
-            note.noteId === noteState.activeNote.noteId &&
-            note.imgUrls.length >= 1
-          ) {
-            return {
-              ...note,
-              imgUrls: [action.payload.secure_url, ...note.imgUrls]
-            };
-          } else {
-            return note;
-          }
-        });
+        // noteState.allNotes = noteState.allNotes.map((note) => {
+        //   if (
+        //     note.noteId === noteState.activeNote.noteId &&
+        //     note.imgUrls.length === 0
+        //   ) {
+        //     return {
+        //       ...note,
+        //       imgUrls: [action.payload.secure_url]
+        //     };
+        //   } else if (
+        //     note.noteId === noteState.activeNote.noteId &&
+        //     note.imgUrls.length >= 1
+        //   ) {
+        //     return {
+        //       ...note,
+        //       imgUrls: [action.payload.secure_url, ...note.imgUrls]
+        //     };
+        //   } else {
+        //     return note;
+        //   }
+        // });
+        //!cosas que voy a necesitar más adelante. End
       }
     );
 
-    builder.addCase(uploadImageThunk.rejected, (noteState) => {
-      noteState!.httpInfo.hasError = true;
-      noteState!.httpInfo.errorMessage = 'error';
-      noteState!.httpInfo.isFetching = false;
-      noteState.dbSavingMessage = '';
-      noteState.dbSavingMessage = 'saving-image-failed';
+    builder.addCase(uploadImageThunk.rejected, (state, action) => {
+      //Petición http
+      state!.httpInfo.isFetching = false;
+      state!.httpInfo.hasError = true;
+      state!.httpInfo.errorMessage = action.error.message as string;
+
+      //Note informacion
+      state.isSavingInDB = false;
+      state.dbSavingMessage = '';
+      // noteState.isThereActiveNote = noteState.isThereActiveNote;
+
+      //active note
+      // noteState.activeNote.body = noteState.activeNote.body;
+      // noteState.activeNote.date = noteState.activeNote.date;
+      // noteState.activeNote.title = noteState.activeNote.title;
+      // noteState.activeNote.noteId = noteState.activeNote.noteId;
+      // noteState.activeNote.imgUrls = noteState.activeNote.imgUrls;
+
+      //all notes
+      // noteState.allNotes = noteState.allNotes;
     });
 
-    builder.addCase(uploadImageThunk.pending, (noteState) => {
-      noteState!.httpInfo.hasError = false;
-      noteState!.httpInfo.errorMessage = null;
-      noteState!.httpInfo.isFetching = true;
-      noteState.dbSavingMessage = 'saving-image-in-process';
+    builder.addCase(uploadImageThunk.pending, (state) => {
+      //Petición http
+      state!.httpInfo.isFetching = true;
+      state!.httpInfo.hasError = false;
+      state!.httpInfo.errorMessage = null;
+
+      //Note informacion
+      state.isSavingInDB = true;
+      state.dbSavingMessage = '';
+      // noteState.isThereActiveNote = noteState.isThereActiveNote;
+
+      //active note
+      // noteState.activeNote.body = noteState.activeNote.body;
+      // noteState.activeNote.date = noteState.activeNote.date;
+      // noteState.activeNote.title = noteState.activeNote.title;
+      // noteState.activeNote.noteId = noteState.activeNote.noteId;
+      // noteState.activeNote.imgUrls = noteState.activeNote.imgUrls;
+
+      //all notes
+      // noteState.allNotes = noteState.allNotes;
     });
   }
 });
