78499627207485ed328b501a94294836b7e080f1
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-06-17 13:03:19 -0500

    Voy a mandar ese problema para la puta mierda y voy a continuar la life.

--- src/__test__/Store/Slices/AuthSlice.test.ts
+++ src/__test__/Store/Slices/AuthSlice.test.ts
@@ -1,5 +1,7 @@
+import { authSlice } from '../../../Store/Slices/Auth/AuthSlice';
 describe('Pruebas en AuthSlice', () => {
   test('Debe llamarse "AuthState"', () => {
+    authSlice.name;
     expect(666).toBe(667);
   });
 });
