dfc01828c50cdfbce0807d3d1b772c741a539ab7
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-06-17 09:09:40 -0500

    Pruebas en el componente que sube imágenes a cloudinary -> Fullhd.

--- src/__test__/Cloudiary/CloudinaryPost.test.ts
+++ src/__test__/Cloudiary/CloudinaryPost.test.ts
@@ -70,16 +70,19 @@ describe('Test on "cloudinaryMultiplePost async function"', () => {
     expect(result).toBe(undefined);
   });
   test('Sube correctamente 1 solo archivo / uploads correctly 1 single archive', async () => {
-    const imgUrl =
-      'https://images.unsplash.com/photo-1500964757637-c85e8a162699?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhbmRzY2FwZXxlbnwwfHwwfHx8MA%3D%3D';
-
+    //creo la imagen.
     const mockFile1 = await createFileFromUrl(imgUrl, 'test-image-1');
+    //creo el arreglo de imágenes.
     const mockFileList = createMockFileList([mockFile1]);
 
+    //subo el arreglo de imágenes de cloudinary.
     const response = await cloudinaryMultiplePost(mockFileList);
     response;
 
+    //casteo la respuesta para que permita trabajar.
     const realResult = response as any as AxiosResponse<CloudinarySuccessed>[];
+
+    //? Empiezo los tests
     expect(realResult).toHaveLength(1);
     expect(realResult[0].data).toHaveProperty('secure_url');
     expect(realResult[0].data.secure_url).toStrictEqual(expect.any(String));
@@ -95,30 +98,42 @@ describe('Test on "cloudinaryMultiplePost async function"', () => {
         response as any as AxiosResponse<CloudinarySuccessed>[]
       )[0].data.secure_url.match(/cloudinary/g)
     ).toBeTruthy();
+    //?Finalizo los tests.
 
-    //Borrar el archivo de cloudinary.
-    // Dividir por '/' y obtener la última parte
+    //?Borrar el archivo de cloudinary.
+    // Dividir por '/'
     const imageUrLparts = realResult[0].data.secure_url.split('/');
+    //  obtener la última parte
     const imageLastUrlPart = imageUrLparts[imageUrLparts.length - 1];
-
     // Quitar la extensión del archivo
     const imageUrlId = imageLastUrlPart.split('.')[0];
 
+    //hago la elimicación en cloudinary con los URLS
     sdkCloudinary.api.delete_resources([imageUrlId]);
   }, 10000);
 
   test('Sube correctamente 3 archivos / uploads correctly 3 archives', async () => {
+    //creo las imágenes.
     const mockFile1 = await createFileFromUrl(imgUrl, 'test-image-1');
     const mockFile2 = await createFileFromUrl(imgUrl, 'test-image-2');
     const mockFile3 = await createFileFromUrl(imgUrl, 'test-image-3');
 
+    //creo el arreglo de imágenes.
     const mockFileList = createMockFileList([mockFile1, mockFile2, mockFile3]);
 
+    //subo las imágenes a cloudinary.
     const response = await cloudinaryMultiplePost(mockFileList);
     response;
 
+    //casteo la response para que me deje trabajar.
     const realResult = response as any as AxiosResponse<CloudinarySuccessed>[];
+    //?Hago los tests.
+
     expect(realResult).toHaveLength(3);
+
+    expect(realResult[0].status).toBe(200);
+    expect(realResult[1].status).toBe(200);
+    expect(realResult[2].status).toBe(200);
 
     expect(realResult[0].data).toHaveProperty('secure_url');
     expect(realResult[1].data).toHaveProperty('secure_url');
@@ -138,8 +153,32 @@ describe('Test on "cloudinaryMultiplePost async function"', () => {
       /^https:\/\/res\.cloudinary\.com/
     );
 
-    expect(realResult[0].status).toBe(200);
-    expect(realResult[1].status).toBe(200);
-    expect(realResult[2].status).toBe(200);
-  });
+    //?Finalizo los tests
+
+    //!Borrar el archivo de cloudinary.
+    //?Image1
+    // Dividir por '/'
+    const image1UrLparts = realResult[0].data.secure_url.split('/');
+    //  obtener la última parte
+    const image1LastUrlPart = image1UrLparts[image1UrLparts.length - 1];
+    // Quitar la extensión del archivo
+    const image1UrlId = image1LastUrlPart.split('.')[0];
+    //?Image2
+    // Dividir por '/'
+    const image2UrLparts = realResult[1].data.secure_url.split('/');
+    //  obtener la última parte
+    const image2LastUrlPart = image2UrLparts[image1UrLparts.length - 1];
+    // Quitar la extensión del archivo
+    const image2UrlId = image2LastUrlPart.split('.')[0];
+    //?Image2
+    // Dividir por '/'
+    const image3UrLparts = realResult[2].data.secure_url.split('/');
+    //  obtener la última parte
+    const image3LastUrlPart = image3UrLparts[image1UrLparts.length - 1];
+    // Quitar la extensión del archivo
+    const image3UrlId = image3LastUrlPart.split('.')[0];
+
+    //hago la elimicación en cloudinary con los URLS
+    sdkCloudinary.api.delete_resources([image1UrlId, image2UrlId, image3UrlId]);
+  }, 10000);
 });
